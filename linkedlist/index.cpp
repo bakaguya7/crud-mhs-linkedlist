4cc01690dc19ebbc3ca4dac9312363df035ff3f6
bakaguya <sucichapany@gmail.com>
2022-07-01 15:14:02 +0700

    commit nh

--- linkedlist/index.cpp
+++ linkedlist/index.cpp
@@ -1,61 +1,129 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 // Var global
-int i;
+int i, nm;
 char q;
 
 struct Mahasiswa {
-    int id;
-    char nim[20];
-    char nama[20];
+    char nim[15];
+    char nama[15];
     float ip_sd;
     float ip_hs;
     float ip_so;
     float ipk;
-    struct Mahasiswa *next;
+    Mahasiswa *next;
 };
-
-Mahasiswa *mhs = new Mahasiswa[10];
+Mahasiswa *head = NULL;
 
 // Deklarasi fungsi
 bool empty();
 bool full();
+void clear();
 void buat();
 void tampil();
+void sorting();
 
 int main(){
-    mhs->id = -1;
     do {
         cout << "[MAIN MENU]" << endl;
-        cout << "1. BUAT \t 2. TAMPIL" << endl;
+        cout << "1. BUAT \t 2. TAMPIL \t 3. SORTING" << endl; 
+        cout << "MASUKKAN NOMOR MENU : "; cin >> nm;
+        switch(nm){
+            case 1 :
+                clear(); buat();
+            break;
+            case 2 :
+                clear(); tampil();
+            break;
+            case 3 :
+                clear(); sorting();
+            break;
+        }
     } while(q != '5');
     
 }
 
-bool empty(){
-    if(mhs->id == -1){
-        return true;
-    } else {
-        return false;
-    }
-}
-
-bool full(){
-    if(mhs->id == 10){
-        return true;
-    } else {
-        return false;
-    }
+void clear(void){
+    system("clear || cls");
+    return;
 }
 
 void buat(){
-    if(full() == true){
-        cout << "DATA PENUH!" << endl;
+    cout << "[MAIN MENU/BUAT]" << endl;
+    do {
+        Mahasiswa *newnode = new Mahasiswa;
+        cout << "MASUKKAN NIM : "; cin.ignore(); cin.getline(newnode->nim, sizeof(newnode->nim));
+        cout << "MASUKKAN NAMA : "; cin.getline(newnode->nama, sizeof(newnode->nama));
+        cout << "MASUKKAN INDEKS PRESTASI SD : "; cin >> newnode->ip_sd;
+        cout << "MASUKKAN INDEKS PRESTASI SO : "; cin >> newnode->ip_so;
+        cout << "MASUKKAN INDEKS PRESTASI HS : "; cin >> newnode->ip_hs;
+        newnode->ipk = (newnode->ip_sd + newnode->ip_so + newnode->ip_hs) / 3;
+        cout << endl;
+
+        if(head == NULL){
+            head = newnode;
+            newnode->next = NULL;
+        } else {
+            newnode->next = head;
+            head = newnode;
+        }
+
+        cout << "BUAT DATA LAGI? [y/n] "; cin >> q;
+    } while(q != 'n');
+}
+
+void tampil(){
+    cout << "[MAIN MENU/TAMPIL]" << endl;
+    if(head == NULL){
+        cout << "DATA KOSONG!" << endl;
     } else {
         do {
-            //
-        } while(q != 'y');
+            Mahasiswa *current = head;
+            cout << "=================================================================" << endl;
+            cout << "| NIM \t\t| NAMA \t\t| IP SD | SO \t| HS \t| IPK \t|" << endl;
+            cout << "=================================================================" << endl;
+            while(current != NULL){
+                cout << "| "<< current->nim << "\t\t| " << current->nama << " \t| "
+                << fixed << setprecision(2) << current->ip_sd << " \t| " << current->ip_so << " \t| "
+                << current->ip_hs << " \t| " << current->ipk << " \t|" << endl;
+                cout << "=================================================================" << endl;
+                current = current->next;
+            }
+            cout << "KEMBALI KE MAIN MENU : [y/n] "; cin >> q;
+        } while(q != 'n');
+    }
+}
+
+void sorting(){
+    cout << "[MAIN MENU/SORTING]" << endl;
+    if(head == NULL){
+        cout << "DATA KOSONG!" << endl;
+    } else {
+        do {
+            Mahasiswa *current = head, *indeks = NULL;
+            // char t_nim[15], t_nama[15];
+            // float t_ipsd, t_ipso, t_iphs, t_ipk;
+            while(current != NULL){
+                indeks = current->next;
+                while(indeks != NULL){
+                    if(current->ipk > indeks->ipk){
+                        swap(current->nim, indeks->nim);
+                        swap(current->nama, indeks->nama);
+                        swap(current->ip_sd, indeks->ip_sd);
+                        swap(current->ip_so, indeks->ip_so);
+                        swap(current->ip_hs, indeks->ip_hs);
+                        swap(current->ipk, indeks->ipk);
+                    }
+                    indeks = indeks->next;
+                }
+                current = current->next;
+            }
+
+            cout << "SORTING BERHASIL!" << endl;
+            cout << "KEMBALI KE MAIN MENU? [y/n] "; cin >> q;
+        } while(q != 'n');
     }
 }
